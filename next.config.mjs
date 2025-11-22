a5869a657e20e56338b710692f949f40d0c67efe
Will Josué <williamjosuedasilva@gmail.com>
2025-11-22 20:44:17 -0300

    Correção para Vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  // Removemos a linha output: 'export' para funcionar na Vercel
 };
 
 export default nextConfig;
